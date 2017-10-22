79ea0221c2db46bf284fe6f4a9f6434a65b81219
Reed Martz <reed@reedmartz.com>
2017-10-22 13:51:21 -0400

    Hide login form with token

--- base.js
+++ base.js
@@ -68,6 +68,7 @@ function average_by_decade(response) {
 
 var logged_in = new Promise(function(resolve, reject) {
     window.addEventListener('load', function () {
+        console.log("Checking for existing token");
         if(localStorage.token) {
             console.log("Using saved token");
             resolve(localStorage.token);
@@ -75,6 +76,7 @@ var logged_in = new Promise(function(resolve, reject) {
         }
         // If there isn't a saved token, wait for the user log-in
         $("#login_form").submit(function() {
+            console.log("Logging in to API server");
             return get_api_token(this).then(function(response) {
                 console.log(response);
                 localStorage.token = response.token;
@@ -84,13 +86,16 @@ var logged_in = new Promise(function(resolve, reject) {
     });
 });
 var city_promise = make_api_request('city/nearest/',
-        {
-            'lat': 39.9526,
-            'lon': -75.1652
-        }).then(function(response) {
-            var city_json = response.features[0];
-            return {
-                id: city_json.id,
-                name: city_json.properties.name
-            };
-        });
+    {
+        'lat': 39.9526,
+        'lon': -75.1652
+    }).then(function(response) {
+        var city_json = response.features[0];
+        return {
+            id: city_json.id,
+            name: city_json.properties.name
+        };
+    });
+logged_in.then(function() {
+    $("#credentials").hide();
+})
