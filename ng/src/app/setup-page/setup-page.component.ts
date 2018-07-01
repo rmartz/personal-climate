d974ae0993bfc8cc4dd4aec669641763bb5785e7
Reed Martz <reed@reedmartz.com>
2018-07-01 11:08:01 -0400

    Add token validation logic

--- ng/src/app/setup-page/setup-page.component.ts
+++ ng/src/app/setup-page/setup-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ConnectionBackend, Headers, Http, Request, RequestOptions, RequestOptionsArgs,
+    Response, URLSearchParams } from '@angular/http';
 
 @Component({
   selector: 'app-setup-page',
@@ -6,6 +8,20 @@ import { Component } from '@angular/core';
 })
 export class SetupPageComponent {
 
-  constructor() { }
+  public token: String = ""
+  public tokenValid: Boolean;
 
+  constructor(protected http: Http) { }
+
+  public verifyApiToken() {
+    const options = new RequestOptions();
+    options.headers = new Headers();
+    options.headers.set('Authorization', 'Token ' + this.token);
+    options.headers.set('Accept', 'application/json');
+
+    const url = 'https://app.climate.azavea.com/api/dataset/'
+    this.http.get(url, options).subscribe(() => {
+        this.tokenValid = true;
+    })
+  }
 }
