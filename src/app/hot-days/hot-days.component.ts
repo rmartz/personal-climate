6f539772ffa266987fed28f60f7c447a883a3a98
Reed Martz <reed@reedmartz.com>
2018-07-09 07:37:02 -0400

    Restructure hot days observable to avoid repeating HTTP request

--- src/app/hot-days/hot-days.component.ts
+++ src/app/hot-days/hot-days.component.ts
@@ -31,43 +31,38 @@ export class HotDaysComponent implements OnInit {
       switchMap<City, Observable<IndicatorData[]>>(city => {
         // We need to store the percentile observer, since the two threshold indicators depend on
         // its value below
-        const percentile_observable = this.climateData.get_indicator_data(city, 'percentile_high_temperature', {
+        return this.climateData.get_indicator_data(city, 'percentile_high_temperature', {
             percentile: Math.floor(100.0 * (365.0 - this.hottest_n) / 365.0)
-          });
-        return combineLatest(
-          percentile_observable.pipe(
+          }).pipe(
             tap(response => {
               this.static_percentile = response;
-            })
-          ),
-          // Number of days that will exceed the current Top N temperature
-          percentile_observable.pipe(
-            switchMap<IndicatorData, IndicatorData>(response => {
-              return this.climateData.get_indicator_data(city, 'max_temperature_threshold', {
-                threshold: response.future,
-                threshold_units: 'F',
-                threshold_comparator: 'gte'
-              });
             }),
-            tap(response => {
-              this.future_threshold = response;
+            switchMap<IndicatorData, IndicatorData>(percentile => {
+              return combineLatest(
+                // Number of days that will exceed the current Top N temperature
+                this.climateData.get_indicator_data(city, 'max_temperature_threshold', {
+                  threshold: percentile.future,
+                  threshold_units: 'F',
+                  threshold_comparator: 'gte'
+                }).pipe(
+                  tap(response => {
+                    this.future_threshold = response;
+                  })
+                ),
+                // Number of days that currently exceed the projected future Top N temperature
+                this.climateData.get_indicator_data(city, 'max_temperature_threshold', {
+                  threshold: percentile.current,
+                  threshold_units: 'F',
+                  threshold_comparator: 'gte'
+                }).pipe(
+                  tap(response => {
+                    this.current_threshold = response;
+                  })
+                )
+              );
             })
-          ),
-          // Number of days that currently exceed the projected future Top N temperature
-          percentile_observable.pipe(
-            switchMap<IndicatorData, IndicatorData>(response => {
-              return this.climateData.get_indicator_data(city, 'max_temperature_threshold', {
-                threshold: response.current,
-                threshold_units: 'F',
-                threshold_comparator: 'gte'
-              });
-            }),
-            tap(response => {
-              this.current_threshold = response;
-            })
-          ),
         );
-      }),
+      })
     ).subscribe();
   }
 }
